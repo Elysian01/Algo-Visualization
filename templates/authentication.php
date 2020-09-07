1b7fa4b408d0d46cd44381dd8cca49e93df6af97
Abhishek Gupta <abhig0209@gmail.com>
2020-09-07 10:35:45 +0530

    validating email part completed

--- templates/authentication.php
+++ templates/authentication.php
@@ -33,18 +33,21 @@
 
             <form action="authentication.php" class="input-group" id="register" method="POST">
                 <input type="text" name="name" class="input-field" placeholder="Name" required>
-                <input type="email" name="email" class="input-field" placeholder="Email" required>
+                <input type="email" name="email" id="email" class="input-field" placeholder="Email" required>
                 <input type="password" name="password" class="input-field" placeholder="Password" required>
-                <button class="submit-btn" type="submit" name="register">Register</button>
+                <button class="submit-btn" type="submit" name="register" onclick="return validateEmail()">Register</button>
+                <!-- <div class="execute" style="visibility: hidden;"></div> -->
             </form>
         </div>
     </div>
 
 
-    <script>
+    <script type="text/javascript">
         var loginForm = document.getElementById("login")
         var registerForm = document.getElementById("register")
         var shiftBtn = document.getElementById("btn")
+        var mail = document.getElementById("email").value;
+        var execute = document.querySelector(".execute");
 
         function register() {
             loginForm.style.left = "-400px";
@@ -57,10 +60,23 @@
             registerForm.style.left = "450px";
             shiftBtn.style.left = "0";
         }
+
+        function validateEmail(e) {
+            var regx = /^([a-zA-Z\.-_]+)@([a-z0-9-]+).([a-z]{2,8})$/;
+            if (regx.test(mail)) {
+                window.open('authentication.php', '_self')
+                return true;
+            }
+            e.preventDefault();
+            alert("You have entered an invalid email address!");
+            mail.focus();
+            return false;
+        }
     </script>
 </body>
 
 </html>
+
 
 <?php
 
@@ -103,33 +119,39 @@ if (isset($_POST['login'])) {
     echo "<script>window.open('index.php','_self')</script>";
 }
 
+
 if (isset($_POST['register'])) {
     $name = mysqli_real_escape_string($con, $_POST['name']);
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
 
-    $ciphering = "AES-128-CTR";
-    $iv_length = openssl_cipher_iv_length($ciphering);
-    $options = 0;
-    $encryption_iv = '2345678910111211';
-    $encryption_key = "DE";
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $ciphering = "AES-128-CTR";
+        $iv_length = openssl_cipher_iv_length($ciphering);
+        $options = 0;
+        $encryption_iv = '2345678910111211';
+        $encryption_key = "DE";
 
-    $encryption = openssl_encrypt(
-        $password,
-        $ciphering,
-        $encryption_key,
-        $options,
-        $encryption_iv
-    );
+        $encryption = openssl_encrypt(
+            $password,
+            $ciphering,
+            $encryption_key,
+            $options,
+            $encryption_iv
+        );
 
-    $query = "insert into auth (name,email,password) values ('$name','$email','$encryption')";
-    $run_register_query = mysqli_query($con, $query);
+        $query = "insert into auth (name,email,password) values ('$name','$email','$encryption')";
+        $run_register_query = mysqli_query($con, $query);
 
-    if ($run_register_query) {
-        echo "<script>alert('SucessFully Registered');</script>";
-        echo "<script>window.open('authentication.php','_self')</script>";
+        if ($run_register_query) {
+            echo "<script>alert('SucessFully Registered');</script>";
+            echo "<script>window.open('authentication.php','_self')</script>";
+        } else {
+            echo "<script>alert('Server Error Please Try Again Later !');</script>";
+        }
     } else {
-        echo "<script>alert('Server Error Please Try Again Later !');</script>";
+        echo "<script>alert('Please Enter Valid Email!');</script>";
     }
 }
+
 ?>
